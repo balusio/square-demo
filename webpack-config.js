aaf9b4c7f66a9c42361c2de5afaceb134c9add2a
Jorge Manzano <jmanzano@everfi.com>
2020-03-26 17:41:54 -0300

    Added basic configs

---
+++ webpack-config.js
@@ -0,0 +1,48 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        enforce: 'pre',
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'eslint-loader',
+          options: {
+            emitError: true,
+          },
+        }],
+      },     
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  output: {
+    path: __dirname + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      minify: {
+        collapseWhitespace: true,
+      },
+      hash: true,
+      template: path.resolve(__dirname, '/src/assets/index.html'),
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 4200,
+  },
+};
